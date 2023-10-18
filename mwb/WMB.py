291e8a567e2ce412b986f82b4fe5f115f7fa34ec
lucasespargueta <lukina7529@gmail.com>
2023-10-18 23:02:45 +0100

    Fixed almost everything

--- mwb/WMB.py
+++ mwb/WMB.py
@@ -1,41 +1,55 @@
 import random
 import math
 import matplotlib.pyplot as plt
+from decimal import Decimal, getcontext
 
-#Esta função calcula e a atualiza a lista de energia para as 1000 partículas
+# Esta função calcula e a atualiza a lista de energia para as 1000 partículas
 def colisions(particles): 
     p1 = random.randint(a=0, b=999)
     p2 = random.randint(a=0, b=999)
-    particles[p1] += 1
-    particles[p2] -= 1
+    if particles[p1]<100 and particles[p2]>1:
+        particles[p1] += 1
+        particles[p2] -= 1
     return(particles)
 
-#Lista de partículas todas com 10 de energia
+# Lista de partículas todas com 10 de energia
 particles = [10] * 1000
 
-#For loop que usa o input do utilizador para os cálculos
-user_input = int(input("Número de colisões (entre 0 e 65536): "))
-if user_input <= 65536:
-    for i in range(user_input):
-        particles = colisions(particles)
-else: 
-    exit()
+# For loop que usa o input do utilizador para os cálculos
+user_input = int(input("Número de colisões: "))
+for i in range(user_input):
+    particles = colisions(particles)
 
-#Imprime a lista de partículas de modo a mostrar as suas energias/nivél energético
+# Imprime a lista de partículas de modo a mostrar as suas energias/nivél energético
 print(particles)
 
-#Faz o cálculo da equção de Maxwell-Boltzman
-i_input = int(input('Nível energético: '))
+# getcontext() é usado para aumentar a precisão dos cálculos, neste caso para 2000 casas decimais
+getcontext().prec = 2000
+
+# Faz o cálculo da equção de Maxwell-Boltzman
 def maxboltz():
-    i = particles.count(i_input)
-    a = int(math.factorial(1000) * (1**i)//math.factorial(i))
-    return(f"O valor de WMB é {math.log(a, math.e)} e a quantidade de partículas no nivél energético {i_input} é de {i}")
+    counter = 1
+    # Este for loop calcula o produtório de cada nivél energético, dividindo 1 (degenerescência) pelo fatorial do número de partículas com esse nivél energético
+    for i in range(1,101):
+        particles_count = particles.count(i)
+        if particles_count != 0:
+            counter *= Decimal((1)/math.factorial(particles_count))
+        else: continue
+    soma = 0
+    # Este for loop calcula o ln de WMB, para isso separa a multiplicação do logaritmo de 1000! em soma de logaritmos de 1 a 1000
+    for i in range(1, len(particles)+1):
+        soma += Decimal(math.log(i))
+    # soma é o ln de WMB
+    soma += Decimal.ln(counter)
+    return(f"O valor de ln(WMB) é {soma.__round__(2)}.")
 
 print(maxboltz())
 
-#Desenha um gráfico de modo a ver se a energia das partículas obdece à distribuição normal
+# Desenha um gráfico de modo a ver se a energia das partículas obdece à distribuição
 fig, ax = plt.subplots()
-ax.hist(particles, rwidth=0.5)
-plt.xlabel("Nivél energético")
+ax.hist(particles, rwidth=0.8, bins=100)
+plt.xticks(range(0, 101, 10))
+plt.title("Distribuição de energia das partículas")
+plt.xlabel("Nível energético")
 plt.ylabel("Quantidade de partículas")
 plt.show()
